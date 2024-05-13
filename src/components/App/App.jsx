8d2708980f93ac29af9b394074fa3ac2276c52bb
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-05-13 23:32:49 +0300

    technical part first exercise

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,7 @@
-import { useState } from 'react'
+
 import './App.css'
 import Profile from '../Profile/Profile'
-
-const userData = {
-  username: "Jacques Gluke",
-  tag: "jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308
-  }
-};
+import userData from '../userData.json'
 
 
 const App = () => {
