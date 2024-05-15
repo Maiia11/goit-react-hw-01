c009fe9089b5b9302a4f436c2b58970f90a300c2
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-05-15 22:52:21 +0300

    style exs1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,24 +1,26 @@
-const Product = ({name, tag, location, image, stats}) => {
+import css from "./Profile.module.css";
+
+const Product = ({ name, tag, location, image, stats }) => {
     return (
-        <div>
-            <div>
-                <img
+        <div className={css.container}>
+            <div className={css.card}>
+                <img className={css.imgCard}
                     src={image}
                     alt="User avatar" />
                 <p>{name}</p>
-                <p>@{tag}</p>
-                <p>{location}</p>
+                <p className={css.textCard}>@{tag}</p>
+                <p className={css.textCard}>{location}</p>
             </div>
-            <ul>
-                <li>
+            <ul className={css.ulCard}>
+                <li className={css.liCard}>
                     <span>Followers</span>
                     <span>{stats.followers}</span>
                 </li>
-                <li>
+                <li className={css.liCard}>
                     <span>Views</span>
                     <span>{stats.views}</span>
                 </li>
-                <li>
+                <li className={css.liCard}>
                     <span>Likes</span>
                     <span>{stats.likes}</span>
                 </li>
